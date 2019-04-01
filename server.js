4ba2ab02cdf59ab794bf9908cae00b1b80bc9922
asd0999 <obelixotic@gmail.com>
2019-04-01 17:14:15 -0400

    four line server

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-var express = require('express')
-var app = express()
-var fs = require("fs");
-app.use(express.static('.'));
-
-app.get('/', function(req, res) {
-  var fileToSend = "index.html";
-  res.sendfile(fileToSend, {
-    root: '.'
-  }); // Files inside "downloads/p5" folder
-});
-
-app.listen(8080, function() {
-  console.log('Example app listening on port 8080!')
-})
-
-app.get('/getfiles', function(req, res) {
-  // res.send('Hello World!');
-  fs.readdir('.', function(err, items) {
-    // console.log(items);
-    res.send(items);
-    // for (var i=0; i<items.length; i++) {
-    //     console.log(items[i]);
-    // }
-  });
-});
+// var express = require('express')
+// var app = express()
+// var fs = require("fs");
+// app.use(express.static('.'));
+//
+// app.get('/', function(req, res) {
+//   var fileToSend = "index.html";
+//   res.sendfile(fileToSend, {
+//     root: '.'
+//   }); // Files inside "downloads/p5" folder
+// });
+//
+// app.listen(8080, function() {
+//   console.log('Example app listening on port 8080!')
+// })
+//
+// app.get('/getfiles', function(req, res) {
+//   // res.send('Hello World!');
+//   fs.readdir('.', function(err, items) {
+//     // console.log(items);
+//     res.send(items);
+//     // for (var i=0; i<items.length; i++) {
+//     //     console.log(items[i]);
+//     // }
+//   });
+// });
 
 //simple four line server
-// var express = require('express');	        // include the express library
-// var server = express();					          // create a server using express
-// server.use('/',express.static('public')); // serve static files from /public
-// server.listen(8080);                      // start the server
+var express = require('express'); // include the express library
+var server = express(); // create a server using express
+server.use('/', express.static('.')); // serve static files from /public
+server.listen(8080); // start the server
